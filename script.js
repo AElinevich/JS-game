fa30add831bdc162f63a7d4150e7ac0388cbd8f1
AElinevich <a.elinevich@tut.by>
2020-06-12 16:14:15 +0300

    with mistakes

--- script.js
+++ script.js
@@ -1,37 +1,45 @@
 // определяем рандомное число  (макс не войдет в диапазон)
-let min = Math.ceil(0);
-let max = Math.floor(101);
-let target = Math.floor(Math.random() * (max - min)) + min;
-console.log(target);
 
 // ловим мусор вместо цифр
 let isNumber = function(n) {
     return !isNaN(parseFloat(n)) && isFinite(n)
 }
 
-function randomNumber() {
-    let question = prompt('Угадай число от 1 до 100', '');
-    
-    if (question === target.toString()) {
-        alert ('Ты угадал');
-}
-    if(question === null) {
-        alert('Игра окончена');     
-    } else {
-        if (question > target) {
+function globalFunc() {
+    let min = Math.ceil(0);
+    let max = Math.floor(101);
+    let target = Math.floor(Math.random() * (max - min)) + min;
+    console.log(target);
+
+
+    function randomNumber(target) {
+        let question = prompt('Угадай число от 1 до 100', '');
+
+   if(question === null) {return console.log(false)}
+   else if(!isNumber(question)) {return randomNumber()}
+   else {
+        question = +question;
+        if(question > target) {
             alert('Загаданное число меньше', '');
             randomNumber();
-
-    }   if(question < target) {
-            alert('Загаданное число больше', '')
+        }
+        else if(question < target) {
+            alert('Загаданное число больше', '');
             randomNumber();
+        } else if (question === target){
+            alert('Ты угадал')
+        }
 
-    }   if (!isNumber(question)) {
-            alert('Введи число', '')
-            randomNumber();
-    }
+
+   }     
+        
 }
+randomNumber(target)
 }
-let two = randomNumber(target);
+
+let call = globalFunc();
+
+
+
 
  
